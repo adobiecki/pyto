163753cb566bc4581bda7109ea1f2659b828354a
adobiecki <arturek4444@gmail.com>
2023-04-30 16:16:41 +0200

    zadania

---
+++ zadanie1.py
@@ -0,0 +1,93 @@
+#print ("witaj w swiecie");
+
+#tekst = "Cześć"
+#print(tekst)
+
+#a = 5
+#b = 6
+#print (a+b)
+
+#a = 5
+#b = 6
+#tekst = "suma="
+#print (tekst)
+#print (a+b)
+
+#x = int(3)
+#y = int(3)
+#z = float(3)
+#print(x)
+#print(y)
+#print(z)
+
+#print(10>9)
+#print(10==9)
+#print(10<9)
+
+#a=100
+#b=10
+#if b<a:
+#    print("tak prawda")
+#else:
+#    print("nie prawda")
+
+#owoce =["jabłko","baban", "pomarańcza"]
+#owoce.sort()
+#print(owoce)
+#liczby = [1,6,2,3,4,5,]
+#liczby.sort()
+#print(liczby)
+#wartosci = ["jeden",1,"dwa",2]
+#print(wartosci)
+
+#a=10
+#b=10
+#if b>a:
+#    print("prawda")
+#elif a==b:
+#    print("liczby są równe")
+#else:
+#    print("to nie prawda")
+
+#i = 1
+#while i <7:
+#    print(i)
+#    if i ==4:
+#     #break
+#     continue
+#    i +=1
+
+
+#bład tu jest nie powino wypisac kiwi
+# #owoce=["pomarańcze","banany","kiwi"]
+#for wypisz in owoce:
+#    print(wypisz)
+#    if wypisz =="banan":
+#        break
+
+#funcje
+#def moja_funkcja():
+#    print("cześć")
+#    
+#moja_funkcja()
+
+#rekurencje silnia
+#def silnia(n):
+#    if n>1:
+#        return n*silnia(n-1)
+#    return 1
+
+#def silnia(n):
+#    if n >1:
+#        return n*silnia(n-1)
+#    else:
+#        return 1
+#n=int(input('n= '))
+#print (n,'!',silnia(n))
+
+#liczba = float(input())
+#print(liczba)
+
+print("podaj swoje imie: ")
+imie=input()
+print("witaj"+imie)
